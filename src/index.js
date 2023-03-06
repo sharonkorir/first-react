7a23b9f4f6ed6ca4767e629386732e4b1a59a06c
sharonkorir <sharon.korir@student.moringaschool.com>
2023-03-06 16:20:15 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Booklist() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
+    </section>
+  );
+}
+
+const books = [
+  {
+    author: "Prince Harry The Duke of Sussex",
+    title: "Spare",
+    img: "./images/book1.jpg",
+  },
+  {
+    author: "Rick Rubin",
+    title: "The Creative Act: A Way of Being",
+    img: "https://m.media-amazon.com/images/I/A1YhM3oRFbL.jpg",
+  },
+];
+
+// const names = ["sharon", "chelangat", "korir"];
+// const newNames = names.map((name) => {
+//   console.log(name);
+//   return <h1>{name}</h1>;
+// });
+
+const Book = ({ img, title, author }) => {
+  // console.log(props);
+
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Booklist />);
