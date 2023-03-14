90881e3b70a2feee0b4bee03a3780c050bb29f96
sharonkorir <sharon.korir@student.moringaschool.com>
2023-03-14 22:06:56 +0300

    set key prop to book id

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ function Booklist() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book img={img} title={title} author={author} />;
+        const { img, title, author, id } = book;
+        // console.log(book);
+        return <Book img={img} title={title} author={author} key={id} />;
       })}
     </section>
   );
@@ -19,20 +20,17 @@ const books = [
     author: "Prince Harry The Duke of Sussex",
     title: "Spare",
     img: "./images/book1.jpg",
+    id: 1,
   },
   {
     author: "Rick Rubin",
     title: "The Creative Act: A Way of Being",
     img: "https://m.media-amazon.com/images/I/A1YhM3oRFbL.jpg",
+    id: 2,
   },
 ];
 
-// const names = ["sharon", "chelangat", "korir"];
-// const newNames = names.map((name) => {
-//   console.log(name);
-//   return <h1>{name}</h1>;
-// });
-
+// book component
 const Book = ({ img, title, author }) => {
   // console.log(props);
 
